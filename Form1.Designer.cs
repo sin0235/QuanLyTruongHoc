a620b04df66499888b71df7b53339a2d68854ea2
phanttuan <tuanphan19405@gmail.com>
2025-04-02 13:59:43 +0700

    tab panel

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,26 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.guna2PanelTitleBar = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2CircleButtonClose = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.guna2CircleButtonMaximize = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.guna2CircleButtonMinimize = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.guna2AnimateWindow1 = new Guna.UI2.WinForms.Guna2AnimateWindow(this.components);
+            this.guna2PanelTitleBar.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // guna2PanelTitleBar
+            // 
+            this.guna2PanelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(240)))), ((int)(((byte)(250)))));
+            this.guna2PanelTitleBar.Controls.Add(this.guna2CircleButtonClose);
+            this.guna2PanelTitleBar.Controls.Add(this.guna2CircleButtonMaximize);
+            this.guna2PanelTitleBar.Controls.Add(this.guna2CircleButtonMinimize);
+            this.guna2PanelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2PanelTitleBar.Location = new System.Drawing.Point(0, 0);
+            this.guna2PanelTitleBar.Name = "guna2PanelTitleBar";
+            this.guna2PanelTitleBar.Size = new System.Drawing.Size(1920, 30);
+            this.guna2PanelTitleBar.TabIndex = 0;
+            this.guna2PanelTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.guna2PanelTitleBar_MouseDown);
+            this.guna2PanelTitleBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.guna2PanelTitleBar_MouseMove);
+            this.guna2PanelTitleBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.guna2PanelTitleBar_MouseUp);
+            // 
+            // guna2CircleButtonClose
+            // 
+            this.guna2CircleButtonClose.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButtonClose.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButtonClose.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2CircleButtonClose.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2CircleButtonClose.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(96)))), ((int)(((byte)(92)))));
+            this.guna2CircleButtonClose.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2CircleButtonClose.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleButtonClose.Location = new System.Drawing.Point(1888, 5);
+            this.guna2CircleButtonClose.Name = "guna2CircleButtonClose";
+            this.guna2CircleButtonClose.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleButtonClose.Size = new System.Drawing.Size(20, 20);
+            this.guna2CircleButtonClose.TabIndex = 3;
+            this.guna2CircleButtonClose.Text = "guna2CircleButtonClose";
+            this.guna2CircleButtonClose.Click += new System.EventHandler(this.guna2CircleButtonClose_Click);
+            this.guna2CircleButtonClose.MouseEnter += new System.EventHandler(this.guna2CircleButtonClose_MouseEnter);
+            this.guna2CircleButtonClose.MouseLeave += new System.EventHandler(this.guna2CircleButtonClose_MouseLeave);
+            // 
+            // guna2CircleButtonMaximize
+            // 
+            this.guna2CircleButtonMaximize.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButtonMaximize.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButtonMaximize.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2CircleButtonMaximize.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2CircleButtonMaximize.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(189)))), ((int)(((byte)(68)))));
+            this.guna2CircleButtonMaximize.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2CircleButtonMaximize.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleButtonMaximize.Location = new System.Drawing.Point(1862, 5);
+            this.guna2CircleButtonMaximize.Name = "guna2CircleButtonMaximize";
+            this.guna2CircleButtonMaximize.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleButtonMaximize.Size = new System.Drawing.Size(20, 20);
+            this.guna2CircleButtonMaximize.TabIndex = 1;
+            this.guna2CircleButtonMaximize.Text = "guna2CircleButtonMaximize";
+            this.guna2CircleButtonMaximize.Click += new System.EventHandler(this.guna2CircleButtonMaximize_Click);
+            this.guna2CircleButtonMaximize.MouseEnter += new System.EventHandler(this.guna2CircleButtonMaximize_MouseEnter);
+            this.guna2CircleButtonMaximize.MouseLeave += new System.EventHandler(this.guna2CircleButtonMaximize_MouseLeave);
+            // 
+            // guna2CircleButtonMinimize
+            // 
+            this.guna2CircleButtonMinimize.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButtonMinimize.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButtonMinimize.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2CircleButtonMinimize.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2CircleButtonMinimize.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(202)))), ((int)(((byte)(78)))));
+            this.guna2CircleButtonMinimize.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2CircleButtonMinimize.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleButtonMinimize.Location = new System.Drawing.Point(1836, 5);
+            this.guna2CircleButtonMinimize.Name = "guna2CircleButtonMinimize";
+            this.guna2CircleButtonMinimize.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleButtonMinimize.Size = new System.Drawing.Size(20, 20);
+            this.guna2CircleButtonMinimize.TabIndex = 2;
+            this.guna2CircleButtonMinimize.Text = "guna2CircleButtonMinimize";
+            this.guna2CircleButtonMinimize.Click += new System.EventHandler(this.guna2CircleButtonMinimize_Click);
+            this.guna2CircleButtonMinimize.MouseEnter += new System.EventHandler(this.guna2CircleButtonMinimize_MouseEnter);
+            this.guna2CircleButtonMinimize.MouseLeave += new System.EventHandler(this.guna2CircleButtonMinimize_MouseLeave);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
             this.ControlBox = false;
+            this.Controls.Add(this.guna2PanelTitleBar);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.guna2PanelTitleBar.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2PanelTitleBar;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButtonMaximize;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButtonMinimize;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButtonClose;
+        private Guna.UI2.WinForms.Guna2AnimateWindow guna2AnimateWindow1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,147 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Guna.UI2.WinForms;
 
 namespace QuanLyTruongHoc
 {
     public partial class Form1 : Form
     {
+        // Lưu trữ màu gốc của các nút
+        private Color closeButtonColor = Color.FromArgb(255, 96, 92); // #FF605C
+        private Color maximizeButtonColor = Color.FromArgb(255, 189, 68); // #FFBD44
+        private Color minimizeButtonColor = Color.FromArgb(0, 202, 78); // #00CA4E
+
         public Form1()
         {
             InitializeComponent();
+
+            // Đảm bảo các nút nằm trong Guna2Panel
+            guna2CircleButtonClose.Parent = guna2PanelTitleBar;
+            guna2CircleButtonMinimize.Parent = guna2PanelTitleBar;
+            guna2CircleButtonMaximize.Parent = guna2PanelTitleBar;
+
+            // Gán màu ban đầu cho các nút
+            guna2CircleButtonClose.FillColor = closeButtonColor;
+            guna2CircleButtonMinimize.FillColor = minimizeButtonColor;
+            guna2CircleButtonMaximize.FillColor = maximizeButtonColor;
+
+            // Đảm bảo các nút hiển thị
+            guna2CircleButtonClose.Visible = true;
+            guna2CircleButtonMinimize.Visible = true;
+            guna2CircleButtonMaximize.Visible = true;
+
+            // Cập nhật vị trí nút ngay khi Form khởi tạo
+            UpdateButtonPositions();
+
+            // Đăng ký sự kiện Resize
+            this.Resize += new EventHandler(Form1_Resize);
+        }
+
+        // Hàm cập nhật vị trí nút
+        private void UpdateButtonPositions()
+        {
+            guna2CircleButtonMinimize.Location = new Point(guna2PanelTitleBar.Width - 90, 5);
+            guna2CircleButtonMaximize.Location = new Point(guna2PanelTitleBar.Width - 60, 5);
+            guna2CircleButtonClose.Location = new Point(guna2PanelTitleBar.Width - 30, 5);
+        }
+
+        // Sự kiện Resize
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            UpdateButtonPositions();
+        }
+
+        // Hiệu ứng hover cho nút Close
+        private void guna2CircleButtonClose_MouseEnter(object sender, EventArgs e)
+        {
+            guna2CircleButtonClose.FillColor = LightenColor(closeButtonColor, 0.2f);
+        }
+
+        private void guna2CircleButtonClose_MouseLeave(object sender, EventArgs e)
+        {
+            guna2CircleButtonClose.FillColor = closeButtonColor;
+        }
+
+        // Hiệu ứng hover cho nút Minimize
+        private void guna2CircleButtonMinimize_MouseEnter(object sender, EventArgs e)
+        {
+            guna2CircleButtonMinimize.FillColor = LightenColor(minimizeButtonColor, 0.2f);
+        }
+
+        private void guna2CircleButtonMinimize_MouseLeave(object sender, EventArgs e)
+        {
+            guna2CircleButtonMinimize.FillColor = minimizeButtonColor;
+        }
+
+        // Hiệu ứng hover cho nút Maximize
+        private void guna2CircleButtonMaximize_MouseEnter(object sender, EventArgs e)
+        {
+            guna2CircleButtonMaximize.FillColor = LightenColor(maximizeButtonColor, 0.2f);
+        }
+
+        private void guna2CircleButtonMaximize_MouseLeave(object sender, EventArgs e)
+        {
+            guna2CircleButtonMaximize.FillColor = maximizeButtonColor;
+        }
+
+        // Hàm làm sáng màu
+        private Color LightenColor(Color color, float factor)
+        {
+            int r = (int)Math.Min(255, color.R + (255 - color.R) * factor);
+            int g = (int)Math.Min(255, color.G + (255 - color.G) * factor);
+            int b = (int)Math.Min(255, color.B + (255 - color.B) * factor);
+            return Color.FromArgb(r, g, b);
+        }
+
+        // Các sự kiện điều khiển nút
+        private void guna2CircleButtonClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void guna2CircleButtonMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void guna2CircleButtonMaximize_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        // Di chuyển Form bằng Guna2Panel
+        private bool isDragging = false;
+        private Point startPoint;
+
+        private void guna2PanelTitleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = true;
+                startPoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void guna2PanelTitleBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point p = PointToScreen(e.Location);
+                this.Location = new Point(p.X - startPoint.X, p.Y - startPoint.Y);
+            }
+        }
+
+        private void guna2PanelTitleBar_MouseUp(object sender, EventArgs e)
+        {
+            isDragging = false;
         }
     }
 }
