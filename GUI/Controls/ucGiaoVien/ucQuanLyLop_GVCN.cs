c6df2df195c3d733521f2aaf620803fac0d0a432
phanttuan <tuanphan19405@gmail.com>
2025-04-25 13:39:02 +0700

    thêm uc cho giáo viên

---
+++ GUI/Controls/ucGiaoVien/ucQuanLyLop_GVCN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyTruongHoc.GUI.Controls.ucGiaoVien
+{
+    public partial class ucQuanLyLop_GVCN : UserControl
+    {
+        public ucQuanLyLop_GVCN()
+        {
+            InitializeComponent();
+        }
+    }
+}
